b412cebd33c7bce48f1f13713e9e9f3fec18fc5d
busraakbulut <busraakbulut@github.com>
2021-01-31 00:13:23 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //Business Codes
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+            new Product{ProductId=1,CategoryId=1,ProductName="Bardak",
+            UnitPrice = 15, UnitsInStock=15},
+            new Product{ProductId=2,CategoryId=1,ProductName="Kamera",
+            UnitPrice = 500, UnitsInStock=3},
+            new Product{ProductId=3,CategoryId=2,ProductName="Telefon",
+            UnitPrice = 1500, UnitsInStock=2},
+            new Product{ProductId=4,CategoryId=2,ProductName="Klavye",
+            UnitPrice = 150, UnitsInStock=65},
+            new Product{ProductId=5,CategoryId=2,ProductName="Fare",
+            UnitPrice = 85, UnitsInStock=1}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //Product productToDelete = null;
+            //foreach (var p in _products)
+            //{
+            //    if (product.productıd == p.productıd)
+            //    {
+            //        producttodelete = p;
+            //    }
+            //}Bir sonraki satır bütün bu işlemleri yapıyor LİNQ sayesinde
+           Product productToDelete = _products.SingleOrDefault(p=>p.ProductId == product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p=>p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {   // gönderdiğim ürün Idsine sahip olan listedeki ürünü bulan program
+            // bu ürüne ulaşmamız gerekiyor ki güncelleyebilelim
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
